40d870f6af0400f1d596b0e2c140d0ddc038ccc9
jhammars <johan.hammarstrm@gmail.com>
2017-09-06 12:45:05 +0000

    Ref comment in pull request

---
+++ js/global-browser-sync.js
@@ -0,0 +1,6 @@
+
+// Following these instructions: https://c9.io/blog/browsersync-gulp-js-cloud9/
+
+//<![CDATA[
+    document.write("<script async src='http://HOST:8082/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+//]]>
